1ca608a20e238a416ed81a41561de678e3c5a5b6
Damian R <damian@motoi.pro>
2025-07-23 07:03:24 +0000

    Checkpoint!

--- lib/std_init.s
+++ lib/std_init.s
@@ -18,8 +18,8 @@
 
 GLABEL init
   DISABLE_INTERRUPTS
-	movea.l  register_data, a5
-	movem.l (a5), d0-d2/a0-a2
+	lea      register_data, a5
+	movem.l  (a5), d0-d2/a0-a2
   // clear RAM
 1:move.l   d0, (a0)+
   dbf      d1, 1b
