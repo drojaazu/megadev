709a617ffe8cb871e7fd3d6ab4d54c3d58e4d001
Damian R <damian@motoi.pro>
2025-08-14 07:46:06 +0000

    Checkpoint!

--- lib/sub/memmap.h
+++ lib/sub/memmap.h
@@ -11,25 +11,11 @@
 #include "sub/memmap.def.h"
 #include "types.h"
 
-#define DMAADDR_WORDRAM1M(addr) (((addr) &0x3FFF) >> 3)
-#define DMAADDR_WORDRAM2M(addr) (((addr) &0x7FFF) >> 3)
-#define DMAADDR_PCM(addr) (((addr) &0x03FF) >> 3)
+#define DMAADDR_WORDRAM1M(addr) (((addr) & 0x3FFF) >> 3)
+#define DMAADDR_WORDRAM2M(addr) (((addr) & 0x7FFF) >> 3)
+#define DMAADDR_PCM(addr) (((addr) & 0x03FF) >> 3)
 #define DMAADDR_PRGRAM(addr) ((addr) >> 3)
 
 #define SP_INT2 (*((void volatile *(*) )(_USERCALL2 + 2)))
-
-// TODO - Verify which registers get trashed
-static inline void bios_waitvsync()
-{
-	asm(
-		"\
-		#move.l a6, -(sp) \n\
-		jsr %p0 \n\
-		#move.l (sp)+, a6 \n\
-		"
-		:
-		: "i"(_WAITVSYNC)
-		: "d0", "d2", "d3", "a0", "a1", "a2");
-}
 
 #endif
