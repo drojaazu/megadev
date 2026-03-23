e7af3d7119cd534d4483d8d9717511ffb7df2e11
Damian R <damian@motoi.pro>
2026-03-23 14:20:51 +0000

    Re-added old vabilla MD/mode 1 work that got lost in the recent git re-shuffle

---
+++ examples/cart/src/res.s
@@ -0,0 +1,5 @@
+
+.section .rodata
+#include "macros.s"
+
+FILE "basic_font.chr", res_basic_font
---
+++ examples/cart_mode1/src/init.s
@@ -0,0 +1,6 @@
+#include "md_vectors.s"
+#include "project.h"
+#include "md_header.s"
+#include "md_init.s"
+
+GLOBAL ERR_BUS testing
