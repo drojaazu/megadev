83a685c59cf718974a1516f760489ec232735994
Damian R <damian@motoi.pro>
2026-04-20 13:54:15 +0000

    Made string compare functions inline

--- lib/str_util.h
+++ lib/str_util.h
@@ -12,12 +12,12 @@
 
 static inline u8 _hextoa(register u8 value)
 {
-	value &= 0x0f;
-	if (value <= 9)
-		value += 0x30;
-	else
-		value += 0x37;
-	return value;
+  value &= 0x0f;
+  if (value <= 9)
+    value += 0x30;
+  else
+    value += 0x37;
+  return value;
 }
 
 /**
@@ -25,11 +25,11 @@ static inline u8 _hextoa(register u8 value)
  */
 static inline void hextoa8(register u8 value, register char * const string)
 {
-	register char * output = string;
-	asm("rol.b #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
-	asm("rol.b #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
+  register char * output = string;
+  asm("rol.b #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
+  asm("rol.b #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
 }
 
 /**
@@ -37,15 +37,15 @@ static inline void hextoa8(register u8 value, register char * const string)
  */
 static inline void hextoa16(register u16 value, register char * const string)
 {
-	register char * output = string;
-	asm("rol.w #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
-	asm("rol.w #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
-	asm("rol.w #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
-	asm("rol.w #4, %[value]" : [value] "+d"(value));
-	*output++ = _hextoa(value);
+  register char * output = string;
+  asm("rol.w #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
+  asm("rol.w #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
+  asm("rol.w #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
+  asm("rol.w #4, %[value]" : [value] "+d"(value));
+  *output++ = _hextoa(value);
 }
 
 /**
@@ -53,38 +53,39 @@ static inline void hextoa16(register u16 value, register char * const string)
  */
 static inline void hextoa32(register u32 value, register char * const string)
 {
-	register char * output = string;
-	for (u8 i = 0; i < 2; ++i)
-	{
-		asm("rol.l #4, %[value]" : [value] "+d"(value));
-		*output++ = _hextoa(value);
-		asm("rol.l #4, %[value]" : [value] "+d"(value));
-		*output++ = _hextoa(value);
-		asm("rol.l #4, %[value]" : [value] "+d"(value));
-		*output++ = _hextoa(value);
-		asm("rol.l #4, %[value]" : [value] "+d"(value));
-		*output++ = _hextoa(value);
-	}
+  register char * output = string;
+  for (u8 i = 0; i < 2; ++i)
+  {
+    asm("rol.l #4, %[value]" : [value] "+d"(value));
+    *output++ = _hextoa(value);
+    asm("rol.l #4, %[value]" : [value] "+d"(value));
+    *output++ = _hextoa(value);
+    asm("rol.l #4, %[value]" : [value] "+d"(value));
+    *output++ = _hextoa(value);
+    asm("rol.l #4, %[value]" : [value] "+d"(value));
+    *output++ = _hextoa(value);
+  }
 }
 
-static bool strcmp(char const * str1, char const * str2)
+static inline bool strcmp(char const * str1, char const * str2)
 {
-	while (*str1 && (*str1 == *str2))
-	{
-		str1++;
-		str2++;
-	}
-	return (*(const unsigned char *) str1 - *(const unsigned char *) str2) == 0;
+  while (*str1 && (*str1 == *str2))
+  {
+    str1++;
+    str2++;
+  }
+  return (*(const unsigned char *) str1 - *(const unsigned char *) str2) == 0;
 }
 
-static bool strcmpt(char const * str1, char const * str2, char terminator)
+static inline bool
+strcmpt(char const * str1, char const * str2, char terminator)
 {
-	while (*str1 != terminator && (*str1 == *str2))
-	{
-		str1++;
-		str2++;
-	}
-	return (*(const unsigned char *) str1 - *(const unsigned char *) str2) == 0;
+  while (*str1 != terminator && (*str1 == *str2))
+  {
+    str1++;
+    str2++;
+  }
+  return (*(const unsigned char *) str1 - *(const unsigned char *) str2) == 0;
 }
 
 #endif
